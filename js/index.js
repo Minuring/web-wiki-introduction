f7665e3969b579858adde95cd296ccdf28878ceb
Minuring <mw9505@gmail.com>
2025-02-26 18:48:02 +0900

    feat: 위키 페이지 작성

--- js/index.js
+++ js/index.js
@@ -1,19 +1,27 @@
-console.log(
-  "%c" +
-    " __      __  ______   __  __   ______     " +
-    "\n" +
-    "/\\ \\  __/\\ \\ /\\__  _\\ /\\ \\ /\\ \\ /\\__  _\\    " +
-    "\n" +
-    "\\ \\ \\/\\ \\ \\ \\/_\\/\\ \\/ \\ \\ \\/'/'\\/ _/\\ \\/    " +
-    "\n" +
-    " \\ \\ \\ \\ \\ \\ \\ \\ \\ \\  \\ \\ , <    \\ \\ \\    " +
-    "\n" +
-    "  \\ \\ \\_/ \\_\\ \\ \\_\\ \\__\\ \\ \\\\`\\   \\_\\ \\__ " +
-    "\n" +
-    "   \\ `\\___x___/ /\\_____\\\\ \\_\\ \\_\\ /\\_____\\ " +
-    "\n" +
-    "    '/__//__/  /_____/ \\/_/\\/_/ /_____/",
-  "color: #d81b60; font-size: 16px; font-weight: bold;"
-);
-
-console.log("알맞은 스크립트를 작성하세요");
+window.onload = function() {
+  const submitButton = document.getElementById("submitButton");
+  submitButton.addEventListener('click', function() {
+    
+    const newCommentText = document.getElementById('commentText').value; // 입력된 댓글 텍스트 가져오기
+    if (newCommentText) {
+        const newLi = document.createElement('li');
+        newLi.innerHTML = `
+            <div class="comment-item">
+                <div class="comment-author">
+                    <img src="./images/comment-author-icon.png" alt="사용자 프로필 이미지" />
+                    <span>방문자</span>
+                </div>
+                <div class="comment-content">
+                    ${newCommentText}
+                </div>
+            </div>
+        `;
+        document.getElementById('comments').appendChild(newLi);
+        document.getElementById('commentText').value = '';
+        alert("댓글이 작성되었습니다.");
+    } else {
+        alert('댓글을 입력하세요!');
+    }
+    
+  });
+}
